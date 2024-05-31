ec405b57e7931c7e94c856b950c8f793ba1a65cc
Sunil-Hegde <sunil1hegde@gmail.com>
2024-05-31 23:25:06 +0530

    fixed count sort

--- sorting/countSort.c
+++ sorting/countSort.c
@@ -1,57 +1,61 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void printArray(int *array,int size){
-    for(int i = 0;i<size;i++){
+void printArray(int *array, int size) {
+    for(int i = 0; i < size; i++) {
         printf("%d ", array[i]);
     }
-    printf("\n\n");
+    printf("\n");
 }
 
-int maximum(int *array, int size){
-    int maxNum = 0;
-    for (int i = 0; i<size;i++){
-        if (array[i] > maxNum){
+int maximum(int *array, int size) {
+    int maxNum = array[0]; 
+    for(int i = 1; i < size; i++) {
+        if(array[i] > maxNum) {
             maxNum = array[i];
         }
     }
     return maxNum;
 }
 
-void countSort(int *array, int size){
+void countSort(int *array, int size) {
     int i, j;
     int max = maximum(array, size);
-    int *count = (int *)calloc(max+1,sizeof(int));
-    for(i = 0;i<size;i++){
+    int *count = (int *)calloc(max + 1, sizeof(int));
+    if (count == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return;
+    }
+
+    for(i = 0; i < size; i++) {
         count[array[i]]++;
     }
-    i = 0, j = 0;
-    while(i<=max){
-        if(count[i] > 0){
-            array[j] = i;
-            count[i]--;
-            j++;
-        } else {
-            i++;
+
+    i = 0;
+    for(j = 0; j <= max; j++) {
+        while(count[j] > 0) {
+            array[i++] = j;
+            count[j]--;
         }
     }
+
+    free(count); 
 }
 
-
-int main(){
+int main() {
     int n;
     printf("Enter number of elements: ");
     scanf("%d", &n);
     int array[n];
     printf("Enter unsorted elements: \n");
-    for(int i = 0; i < n;i++){
+    for(int i = 0; i < n; i++) {
         scanf("%d", &array[i]);
     }
     printf("\nElements in unsorted array:\n");
     printArray(array, n);
 
-    printf("After sorting using Insertion Sort:\n");
-    countSort(array, n); 
+    printf("After sorting using Count Sort:\n");
+    countSort(array, n);
     printArray(array, n);
     return 0;
 }
